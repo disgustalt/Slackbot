b25ae92b16b8dc1044c5cd1d2426dc91655a65a6
Byul <214102256+disgustalt@users.noreply.github.com>
2026-09-22 22:59:32 +0500

    update index.js

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ app.command("/dynamite-ping", async ({ command, ack, respond }) => {const start 
   await respond({ text: `Pong!\nLatency: ${latency}ms` });
 });
 
+app.command("/dynamite-catfact", async({command, ack, respond}) => {
+  await ack();
+  try {
+    const req = await fetch("https://cat.ninja/fact", { method: 'GET' });
+    const res = await req.json();
+    const fact = res.data.fact;
+    await respond({ text: fact });
+  } catch (e) {
+    await respond({ text: "Uh oh, something went wrong \;)" })
+  }
+});
+
 (async () => {
   await app.start();
   console.log("bot is running!");
