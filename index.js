0a7abc790ffdaee89f7fb8b73ddfd8e9c51107d9
Byul <214102256+disgustalt@users.noreply.github.com>
2026-09-23 21:03:16 +0500

    update index.js

--- index.js
+++ index.js
@@ -29,17 +29,5 @@ const load = async () => {
 
 load();
 
-app.command("/dynamite-catfact", async({command, ack, respond}) => {
-  await ack();
-  try {
-    const req = await fetch("https://cat.ninja/fact", { method: 'GET' });
-    const res = await req.json();
-    const fact = res.data.fact;
-    await respond({ text: fact });
-  } catch (e) {
-    await respond({ text: "Uh oh, something went wrong \;)" })
-  }
-});
-
 await app.start();
 console.log("bot is running!");
