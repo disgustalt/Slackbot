b6090df82194a1d17e27c68e4dba86153f5ed620
Byul <214102256+disgustalt@users.noreply.github.com>
2026-09-23 13:13:12 +0500

    update commands/catfact.js

---
+++ commands/catfact.js
@@ -0,0 +1,13 @@
+export default async (app) => {
+  app.command("/dynamite-catfact", async({command, ack, respond}) => {
+    await ack();
+    try {
+      const req = await fetch("https://cat.ninja/fact", { method: 'GET' });
+      const res = await req.json();
+      const fact = res.data.fact;
+      await respond({ text: fact });
+    } catch (e) {
+      await respond({ text: "Uh oh, something went wrong \;)" })
+    }
+  });
+};
