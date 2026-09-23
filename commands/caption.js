74feb01d500713d237f05335aef913e8756cec29
Byul <214102256+disgustalt@users.noreply.github.com>
2026-09-23 18:02:46 +0500

    update commands/caption.js

--- commands/caption.js
+++ commands/caption.js
@@ -1,30 +1,62 @@
 export default async (app) => {
-  app.command("/dynamite-caption", async({ ack, client, command }) => {
-    await ack();
-    await client.views.open({
-      trigger_id: command.trigger_id,
-      view: {
-        type: "modal",
-        callback_id: "captionup",
-        title: {
-          type: "plain_text",
-          text: "Upload"
-        },
-        submit: {
-          type: "plain_text",
-          text: "Submit"
-        },
-        close: {
-          type: "plain_text",
-          text: "Cancel"
-        },
-        blocks: [
-          {
-            type: "input",
-            
-          }
-        ]
-      }
-    });
+  app.command("/dynamite-caption", async({ ack, client, command, respond }) => {
+    try {
+      await ack();
+      await client.views.open({
+        trigger_id: command.trigger_id,
+        view: {
+          type: "modal",
+          callback_id: "captionup",
+          title: {
+            type: "plain_text",
+            text: "Upload"
+          },
+          submit: {
+            type: "plain_text",
+            text: "Submit"
+          },
+          close: {
+            type: "plain_text",
+            text: "Cancel"
+          },
+          blocks: [
+            {
+              type: "input",
+              block_id: "captionblock",
+              element: {
+                type: "plain_text_input",
+                action_id: "captionin",
+                placeholder: {
+                  type: "plain_text",
+                  text: "Enter caption text"
+                }
+              },
+              label: {
+                type: "plain_text",
+                text: "Caption"
+              }
+            },
+            {
+              type: "input",
+              block_id: "imgin",
+              element: {
+                type: "file_input",
+                action_id: "img",
+                filetypes: ["png", "jpg", "jpeg", "webm", "webp"],
+                max_files: 1
+              },
+              label: {
+                type: "plain_text",
+                text: "Image to caption"
+              }
+            }
+          ]
+        }
+      });
+    } catch (e) {
+      await respond({
+        text: "Uh oh, Something went wrong!"
+      });
+    }
   });
 };
