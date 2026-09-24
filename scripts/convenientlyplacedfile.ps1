6050feae6a292c2518d957cf4b7e5febba1782c6
Byul <214102256+disgustalt@users.noreply.github.com>
2026-09-24 21:19:27 +0500

    update scripts/convenientlyplacedfile.ps1

--- scripts/convenientlyplacedfile.ps1
+++ scripts/convenientlyplacedfile.ps1
@@ -2,7 +2,7 @@ if ( Test-Path "repo" ) { Remove-Item -Recurse -Force "repo" }
 
 if ( Test-Path "projects\slackbot\repo" ) { Remove-Item -Recurse -Force "projects\slackbot\repo" }
 
-if ( -not ( Test-Path "projects\slackbot" ) ) { New-Item -ItemType Directory -Path "projects\slackbot" -Force }
+if ( (Get-Location).Path -notlike "*projects\slackbot" and -not ( Test-Path "projects\slackbot" ) ) { New-Item -ItemType Directory -Path "projects\slackbot" -Force }
 
 if ( (Get-Location).Path -notlike "*projects\slackbot" ) { Set-Location "projects\slackbot" }
 
@@ -14,5 +14,12 @@ if ( Test-Path "repo\package.json" ) { Move-Item "repo\package.json" . -Force }
 SLACK_BOT_TOKEN=your_bot(oauth)_token
 SLACK_APP_TOKEN=your_app_token
 '@ | Out-File -FilePath .env -Encoding utf8 -Force
+
+@'
+export default {
+  bot_name: "your_bot_name"
+  prefix: true
+}
+'@ | Out-File repo\config.js -Encoding  utf8 -Force
 
 npm install
