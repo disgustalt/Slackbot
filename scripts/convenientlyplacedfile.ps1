3eba284e4c9af8f700b6793979265d079318b4cf
Byul <214102256+disgustalt@users.noreply.github.com>
2026-09-24 21:08:54 +0500

    update scripts/convenientlyplacedfile.ps1

--- scripts/convenientlyplacedfile.ps1
+++ scripts/convenientlyplacedfile.ps1
@@ -1,7 +1,18 @@
-if (Test-Path "projects\slackbot\repo") { Remove-Item -Recurse -Force "projects\slackbot\repo" }
+if ( Test-Path "repo" ) { Remove-Item -Recurse -Force "repo" }
 
-if (-not (Test-Path "projects\slackbot")) { New-Item -ItemType Directory -Path "projects\slackbot" -Force }
+if ( Test-Path "projects\slackbot\repo" ) { Remove-Item -Recurse -Force "projects\slackbot\repo" }
 
-Set-Location "projects\slackbot"
+if ( -not ( Test-Path "projects\slackbot" ) ) { New-Item -ItemType Directory -Path "projects\slackbot" -Force }
 
-git clone https://
+if ( (Get-Location).Path -notlike "*projects\slackbot" ) { Set-Location "projects\slackbot" }
+
+git clone https://github.com/disgustalt/Slackbot repo
+
+if ( Test-Path "repo\package.json" ) { Move-Item "repo\package.json" . -Force }
+
+@'
+SLACK_BOT_TOKEN=your_bot(oauth)_token
+SLACK_APP_TOKEN=your_app_token
+'@ | Out-File -FilePath .env -Encoding utf8 -Force
+
+npm install
